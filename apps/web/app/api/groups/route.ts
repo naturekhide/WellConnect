657136506ea64a64d4959eb60ae915207e2499d0
Ibrahim Aswad Nindow <nindowa421@gmail.com>
2026-04-19 23:59:15 +0000

    Fix TypeScript error in groups API route

--- apps/web/app/api/groups/route.ts
+++ apps/web/app/api/groups/route.ts
@@ -1,22 +1,6 @@
-/**
- * Copyright 2026 Ibrahim Aswad Nindow
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { auth } from "@/lib/auth";
+import { auth } from "../../../../lib/auth";
 
 const prisma = new PrismaClient();
 
@@ -89,7 +73,6 @@ export async function POST(request: NextRequest) {
       },
     });
 
-    // Automatically add creator as admin
     await prisma.groupMember.create({
       data: {
         userId: session.user.id,
